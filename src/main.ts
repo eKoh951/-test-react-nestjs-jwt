c8d0b9ce5168ef27cc9c7202de26168f57f6dd63
iAsure <asuregames01@gmail.com>
2023-02-05 18:49:56 -0600

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import { AppModule } from './app.module';
 import { auth } from 'express-oauth2-jwt-bearer';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: { origin: 'http://127.0.0.1:5173' },
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true,
   });
-  app.enableCors();
   app.use(
     auth({
       issuer: 'asure.us.auth0.com',
-      audience: 'https://asure.us.auth0.com/api/v2/',
+      audience: 'https://asure.us.auth0.com/api/v2',
       secret: 'dawdawdawd, dokpawdad awd ad',
       tokenSigningAlg: 'HS256',
     }),
