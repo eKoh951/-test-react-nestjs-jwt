d660ca323d1f127f759a9cbb6636b7d776e7d038
iAsure <asuregames01@gmail.com>
2023-02-05 23:39:37 -0600

    cors removed

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
 
   // {
   //   origin: 'http://127.0.0.1:5173',
@@ -10,16 +10,6 @@ async function bootstrap() {
   //   credentials: true,
   //   allowedHeaders: 'Authorization, Content-Type, Accept',
   // }
-
-  // app.use(
-  //   ,
-  // );
-  // const checkJwt = auth({
-  //   issuer: 'asure.us.auth0.com',
-  //   audience: 'https://asure.us.auth0.com/api/v2',
-  //   secret: 'dawdawdawd, dokpawdad awd ad',
-  //   tokenSigningAlg: 'HS256',
-  // });
 
   await app.listen(3000);
 }
